8ff3997368a74c936560c1c25e967689e028f729
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-12-18 13:34:25 +0100

    Tweak the recipes in a more robust way

--- prototypes/recipe/recipe.lua
+++ prototypes/recipe/recipe.lua
@@ -19,49 +19,99 @@ end
 -- Tweaks
 
 -- https://github.com/wube/factorio-data/blob/master/base/prototypes/recipe.lua
-recipes['steel-plate'].normal.energy_required = 6
+if( recipes['steel-plate'] and recipes['steel-plate'].normal )
+then
+  recipes['steel-plate'].normal.energy_required = 6
+end
 -- Science packs, yay! These are "cheap" individually, however, large
 -- quantities of them are required.
-recipes['chemical-science-pack'].result_count = 4
-recipes['military-science-pack'].result_count = 4
-recipes['production-science-pack'].result_count = 6
-recipes['production-science-pack'].ingredients[3][2] = 10
-recipes['utility-science-pack'].result_count = 6
+set_result_count('chemical-science-pack', 4)
+set_result_count('military-science-pack', 4)
+set_result_count('production-science-pack', 6)
+set_ingredient_amount('production-science-pack', 3, 10)
+set_result_count('utility-science-pack', 6)
 -- science packs reconfiguration ends here
-recipes['rocket-control-unit'].result_count = 2
+set_result_count('rocket-control-unit', 2)
 -- Nuclear! This needs massive changes for it to make sense in super cheap mode
-recipes['nuclear-reactor'].ingredients[1][2] = 100
-recipes['nuclear-reactor'].ingredients[2][2] = 100
-recipes['nuclear-reactor'].ingredients[3][2] = 100
-recipes['nuclear-reactor'].ingredients[4][2] = 100
-recipes['centrifuge'].ingredients[1][2] = 20
-recipes['centrifuge'].ingredients[2][2] = 10
-recipes['centrifuge'].ingredients[3][2] = 20
-recipes['centrifuge'].ingredients[4][2] = 20
-recipes['uranium-processing'].energy_required = 1
-recipes['uranium-processing'].ingredients[1][2] = 1 -- 10-times more uranium for us, yay!
-recipes['kovarex-enrichment-process'].energy_required = 12
-recipes['kovarex-enrichment-process'].ingredients[1][2] = 5
-recipes['kovarex-enrichment-process'].ingredients[2][2] = 3
-recipes['kovarex-enrichment-process'].results[1][2] = 6
-recipes['kovarex-enrichment-process'].results[2][2] = 1
-recipes['nuclear-fuel'].energy_required = 40
-recipes['nuclear-fuel-reprocessing'].energy_required = 20
-recipes['nuclear-fuel-reprocessing'].ingredients[1][2] = 3
-recipes['uranium-fuel-cell'].ingredients[1][2] = 2
-recipes['uranium-fuel-cell'].result_count = 20
-recipes['heat-exchanger'].ingredients[1][2] = 1
-recipes['heat-exchanger'].ingredients[2][2] = 10
-if( recipes['heat-exchanger'].ingredients[3] ~= nil )
+set_ingredient_amount('nuclear-reactor', 1, 100)
+set_ingredient_amount('nuclear-reactor', 2, 100)
+set_ingredient_amount('nuclear-reactor', 3, 100)
+set_ingredient_amount('nuclear-reactor', 4, 100)
+set_ingredient_amount('centrifuge', 1, 20)
+set_ingredient_amount('centrifuge', 2, 10)
+set_ingredient_amount('centrifuge', 3, 20)
+set_ingredient_amount('centrifuge', 4, 20)
+if( recipes['uranium-processing'] )
 then
-  recipes['heat-exchanger'].ingredients[3][2] = 1
+  recipes['uranium-processing'].energy_required = 1
 end
-recipes['heat-pipe'].ingredients[1][2] = 1
-recipes['heat-pipe'].ingredients[2][2] = 2
-if( recipes['steam-turbine'].ingredients ~= nil )
+set_ingredient_amount('uranium-processing', 1, 1) -- 10-times more uranium for us, yay!
+set_ingredient_amount('kovarex-enrichment-process', 1, 5)
+set_ingredient_amount('kovarex-enrichment-process', 2, 3)
+local enrichment_recipe = recipes['kovarex-enrichment-process']
+if( enrichment_recipe and enrichment_recipe.results )
 then
-  recipes['steam-turbine'].ingredients[1][2] = 10
-  recipes['steam-turbine'].ingredients[2][2] = 10
-  recipes['steam-turbine'].ingredients[3][2] = 4
+  enrichment_recipe.energy_required = 12
+  enrichment_recipe.results[1][2] = 6
+  enrichment_recipe.results[2][2] = 1
 end
+if( recipes['nuclear-fuel'] )
+then
+  recipes['nuclear-fuel'].energy_required = 40
+end
+if( recipes['nuclear-fuel-reprocessing'] )
+then
+  recipes['nuclear-fuel-reprocessing'].energy_required = 20
+end
+set_ingredient_amount('nuclear-fuel-reprocessing', 1, 3)
+set_ingredient_amount('uranium-fuel-cell', 1, 2)
+set_result_count('uranium-fuel-cell', 20)
+set_ingredient_amount('heat-exchanger', 1, 1)
+set_ingredient_amount('heat-exchanger', 2, 10)
+set_ingredient_amount('heat-exchanger', 3, 1)
+set_ingredient_amount('heat-pipe', 1, 1)
+set_ingredient_amount('heat-pipe', 2, 2)
+set_ingredient_amount('steam-turbine', 1, 10)
+set_ingredient_amount('steam-turbine', 2, 10)
+set_ingredient_amount('steam-turbine', 3, 4)
 -- End of nuclear tweaks
+
+local function set_ingredient_amount(recipe_name, ingredient_index, amount)
+  local recipe = recipes[recipe_name]
+  if( recipe )
+  then
+    local ingredient = nil
+    if( recipe.normal and recipe.normal.ingredients )
+    then
+      ingredient = recipe.normal.ingredients[ingredient_index]
+    elseif( recipe.ingredients )
+    then
+      ingredient = recipe.ingredients[ingredient_index]
+    end
+
+    if( ingredient )
+    then
+      if( ingredient.amount )
+      then
+        ingredient.amount = amount
+      elseif( ingredient[2] )
+      then
+        ingredient[2] = amount
+      end
+    end
+  end
+end
+
+local function set_result_count(recipe_name, result_count)
+  local recipe = recipes[recipe_name]
+  if( recipe )
+  then
+    if( recipe.normal and recipe.normal.result_count )
+    then
+      recipe.normal.result_count = result_count
+    elseif( recipe.result_count )
+    then
+      recipe.result_count = result_count
+    end
+  end
+end
