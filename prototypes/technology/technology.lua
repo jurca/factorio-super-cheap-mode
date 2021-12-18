d7a9e7de5e87b6e650244ce6f2f6f282460011b4
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-12-18 10:55:58 +0100

    Set infinite reasearch formulas more robustly

--- prototypes/technology/technology.lua
+++ prototypes/technology/technology.lua
@@ -10,12 +10,20 @@ for _,technology in pairs(technologies) do
 end
 
 -- https://github.com/wube/factorio-data/blob/master/base/prototypes/technology.lua
-technologies['physical-projectile-damage-7'].unit.count_formula = '2^(L-7)*250'
-technologies['stronger-explosives-7'].unit.count_formula = '2^(L-7)*250'
-technologies['refined-flammables-7'].unit.count_formula = '2^(L-7)*250'
-technologies['energy-weapons-damage-7'].unit.count_formula = '2^(L-7)*250'
-technologies['artillery-shell-range-1'].unit.count_formula = '2^L*250'
-technologies['artillery-shell-speed-1'].unit.count_formula = '250+3^(L-1)*250'
-technologies['follower-robot-count-7'].unit.count_formula = '25(L-6)+225'
-technologies['worker-robots-speed-6'].unit.count_formula = '2^(L-6)*250'
-technologies['mining-productivity-4'].unit.count_formula = '625*(L - 3)'
+set_technology_formula('physical-projectile-damage-7', '2^(L-7)*250')
+set_technology_formula('stronger-explosives-7', '2^(L-7)*250')
+set_technology_formula('refined-flammables-7', '2^(L-7)*250')
+set_technology_formula('energy-weapons-damage-7', '2^(L-7)*250')
+set_technology_formula('artillery-shell-range-1', '2^L*250')
+set_technology_formula('artillery-shell-speed-1', '250+3^(L-1)*250')
+set_technology_formula('follower-robot-count-7', '25(L-6)+225')
+set_technology_formula('worker-robots-speed-6', '2^(L-6)*250')
+set_technology_formula('mining-productivity-4', '625*(L - 3)')
+
+local function set_technology_formula (name, formula)
+  local technology = technologies[name]
+  if( technology and technology.unit and technology.unit.count_formula )
+  then
+    technology.unit.count_formula = formula
+  end
+end
